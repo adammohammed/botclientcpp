f889017d17f4f8940a78e58dac69029f77ae369e
Adam Mohammed <adammoham@gmail.com>
2019-01-22 21:50:39 -0500

    Pistache Test now shows functionality for POST Request with body

--- tests/test_pistache.cpp
+++ tests/test_pistache.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "pistache/endpoint.h"
 
 using namespace Pistache;
@@ -5,23 +6,31 @@ using namespace Pistache;
 class HelloHandler : public Http::Handler {
 public:
 
-    HTTP_PROTOTYPE(HelloHandler)
+  HTTP_PROTOTYPE(HelloHandler)
 
-    void onRequest(const Http::Request& request, Http::ResponseWriter response) {
-        UNUSED(request);
-        response.send(Pistache::Http::Code::Ok, "Hello World\n");
+  void onRequest(const Http::Request& request, Http::ResponseWriter response) {
+    if (request.method() == Http::Method::Get){
+      std::cout << "IT was a get request" << std::endl;
+    } else if( request.method() == Http::Method::Post) {
+      std::cout << "BODY:" << std::endl;
+      std::cout << request.body() << std::endl;
     }
+    response.send(Pistache::Http::Code::Ok, "Hello World\n");
+  }
 };
 
 int main() {
-    Pistache::Address addr(Pistache::Ipv4::any(), Pistache::Port(9080));
-    auto opts = Pistache::Http::Endpoint::options()
-        .threads(1);
+  constexpr int PORT = 9080;
+  Pistache::Address addr(Pistache::Ipv4::any(), Pistache::Port(PORT));
+  auto opts = Pistache::Http::Endpoint::options()
+    .threads(1);
 
-    Http::Endpoint server(addr);
-    server.init(opts);
-    server.setHandler(Http::make_handler<HelloHandler>());
-    server.serve();
+  Http::Endpoint server(addr);
+  server.init(opts);
+  server.setHandler(Http::make_handler<HelloHandler>());
 
-    server.shutdown();
+  std::cout << "Starting server at localhost:" << PORT << std::endl;
+  server.serve();
+
+  server.shutdown();
 }
